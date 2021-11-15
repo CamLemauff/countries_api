a246587c47a94c3f60803f58c5070853f9569f43
Camille Lemauff <camille.lemauff@vo2-group.com>
2021-11-15 16:22:39 -0500

    [ADD] first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container-home">
+        Test
+    </div>
+</template>
+
+<script>
+// import NavbarHome from './headers/NavbarHome.vue';
+export default {
+  name: 'Index',
+//   components: {
+//       NavbarHome
+//   },
+  props: {
+    msg: String
+  }
+}
+</script>
